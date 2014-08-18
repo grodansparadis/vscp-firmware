53216d4ef500cfba580cc080232ff78c6f06b275
Ake Hedman <akhe@grodansparadis.com>
2014-08-18 16:29:20 +0200

    Adopted for MPLABX. fuses now programmed correctly. readme.txt renamed to README for consistency. version.txt renamed to HISTORY for consistency. Distribution files for PIC18F2580, PIC18F2680 (PIC18F25K80, PIC18F26K80) available.
    Hex files available here https://sourceforge.net/projects/m2m/files/VSCP%20Firmware/bootloader/

--- pic/bootloader/Microchip/vscp_can_bootloader_18f/CANBoot.X/nbproject/Package-default.bash
+++ pic/bootloader/Microchip/vscp_can_bootloader_18f/CANBoot.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/CANBootx.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=CANBootx.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=canbootx.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/CANBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=CANBoot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=canboot.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/canbootx.x/bin
+makeDirectory ${TMPDIR}/canboot.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/canbootx.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/canboot.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/canbootx.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/canboot.x.tar *
 checkReturnCode
 
 # Cleanup
